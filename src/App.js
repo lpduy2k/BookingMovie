1dbbdd49a17808aed181b23d567ed87829fbe973
unknown <lp.duy2k@gmail.com>
2020-12-20 14:07:07 +0700

    buoi_1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route} from 'react-router-dom';
+import TrangChu from './pages/TrangChu/TrangChu';
+import DangKy from './pages/DangKy/DangKy';
+import DangNhap from './pages/DangNhap/DangNhap';
+import ChiTietPhim from './pages/ChiTietPhim/ChiTietPhim';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Route path="/trangchu" component={TrangChu}/>
+      <Route path="/dangky" component={DangKy}/>
+      <Route path="/dangnhap" component={DangNhap}/>
+      <Route path="/chitietphim" component={ChiTietPhim}/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,8 @@
+import {combineReducers, createStore} from 'redux';
+
+const rootReducer = combineReducers({
+    // nơi định nghĩa các reducer trong hệ thống
+
+});
+
+export const store = createStore(rootReducer);
